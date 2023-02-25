d174dfcd3e08adc5cd999cffb39096bc804e0516
Glenn <dev-glennn@gmail.com>
2023-02-25 10:34:43 +0900

    Chore: config, package 설정

---
+++ nuxt.config.js
@@ -0,0 +1,68 @@
+export default {
+  // Disable server-side rendering: https://go.nuxtjs.dev/ssr-mode
+  ssr: false,
+
+  head: {
+    title: '레진 프론트 과제 (조민주)',
+    htmlAttrs: {
+      lang: 'ko',
+    },
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { hid: 'description', name: 'description', content: '' },
+      { name: 'format-detection', content: 'telephone=no' },
+    ],
+    link: [
+      { rel: 'icon', type: 'image/x-icon', href: 'https://ccdn.lezhin.com/files/assets/img/favicon.png' },
+      { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css2?family=Noto+Sans+KR:wght@100;300;400;500;700;900&display=swap' }
+    ],
+  },
+
+  // Global CSS: https://go.nuxtjs.dev/config-css
+  css: [
+    '@/assets/style/default.css',
+    '@/assets/style/colors.scss',
+    '@/assets/style/font.scss'
+  ],
+
+  // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
+  plugins: [],
+
+  // Auto import components: https://go.nuxtjs.dev/config-components
+  components: true,
+
+  build: {
+    extend(config, ctx) {
+      if (ctx.dev && ctx.isClient) {
+        config.module.rules.push({
+          enforce: 'pre',
+          test: /\.(js|vue)$/,
+          loader: 'eslint-loader',
+          exclude: /(node_modules)/,
+          options: {
+            fix: true
+          }
+        })
+      }
+    }
+  },
+
+  // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
+  buildModules: [
+  ],
+
+  axios: {
+    headers: {
+      'Content-Type': 'application/json; charset=utf-8'
+    },
+    host: "localhost",
+    port: "4000",
+    prefix: "/api/",
+  },
+
+  // Modules: https://go.nuxtjs.dev/config-modules
+  modules: [
+    "@nuxtjs/axios"
+  ]
+}
